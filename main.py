5189194c626ed439938a4740d605253aaccba447
Mahadi Sajjad Neloy <sierra117@qq.com>
2021-07-31 20:31:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+
 import cv2
-from google.colab.patches import cv2_imshow
 import matplotlib.pyplot as plt
-import skimage
 from skimage import measure, morphology
 from skimage.color import label2rgb
 from skimage.measure import regionprops
+import numpy as np
 
-# Read Image
-img = cv2.imread('in1.jpg', 0)
-img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1] 
-# Extract Blobs
+# the parameters are used to remove small size connected pixels outliar
+constant_parameter_1 = 84
+constant_parameter_2 = 250
+constant_parameter_3 = 100
+
+# the parameter is used to remove big size connected pixels outliar
+constant_parameter_4 = 18
+
+# read the input image
+img = cv2.imread('./inputs/in1.jpg', 0)
+img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]  # ensure binary
+
+# connected component analysis by scikit-learn framework
 blobs = img > img.mean()
 blobs_labels = measure.label(blobs, background=1)
 image_label_overlay = label2rgb(blobs_labels, image=img)
 
-# Area of Text
+fig, ax = plt.subplots(figsize=(10, 6))
+
+'''
+# plot the connected components (for debugging)
+ax.imshow(image_label_overlay)
+ax.set_axis_off()
+plt.tight_layout()
+plt.show()
+'''
+
 the_biggest_component = 0
 total_area = 0
 counter = 0
@@ -23,30 +41,43 @@ for region in regionprops(blobs_labels):
     if (region.area > 10):
         total_area = total_area + region.area
         counter = counter + 1
+    # print region.area # (for debugging)
+    # take regions with large enough areas
     if (region.area >= 250):
         if (region.area > the_biggest_component):
             the_biggest_component = region.area
-# Threshold
+
 average = (total_area/counter)
 print("the_biggest_component: " + str(the_biggest_component))
 print("average: " + str(average))
 
-a4_constant = ((average/84.0)*250.0)+100
-print("a4_constant: " + str(a4_constant))
-# Remove Noise
-b = morphology.remove_small_objects(blobs_labels, a4_constant)
+# experimental-based ratio calculation, modify it for your cases
+# a4_small_size_outliar_constant is used as a threshold value to remove connected outliar connected pixels
+# are smaller than a4_small_size_outliar_constant for A4 size scanned documents
+a4_small_size_outliar_constant = ((average/constant_parameter_1)*constant_parameter_2)+constant_parameter_3
+print("a4_small_size_outliar_constant: " + str(a4_small_size_outliar_constant))
 
-plt.imsave('pre_version.png', b)
+# experimental-based ratio calculation, modify it for your cases
+# a4_big_size_outliar_constant is used as a threshold value to remove outliar connected pixels
+# are bigger than a4_big_size_outliar_constant for A4 size scanned documents
+a4_big_size_outliar_constant = a4_small_size_outliar_constant*constant_parameter_4
+print("a4_big_size_outliar_constant: " + str(a4_big_size_outliar_constant))
+
+# remove the connected pixels are smaller than a4_small_size_outliar_constant
+pre_version = morphology.remove_small_objects(blobs_labels, a4_small_size_outliar_constant)
+# remove the connected pixels are bigger than threshold a4_big_size_outliar_constant
+# to get rid of undesired connected pixels such as table headers and etc.
+component_sizes = np.bincount(pre_version.ravel())
+too_small = component_sizes > (a4_big_size_outliar_constant)
+too_small_mask = too_small[pre_version]
+pre_version[too_small_mask] = 0
+# save the the pre-version which is the image is labelled with colors
+# as considering connected components
+plt.imsave('pre_version.png', pre_version)
 
 # read the pre-version
-img2 = cv2.imread('pre_version.png', 0)
+img = cv2.imread('pre_version.png', 0)
 # ensure binary
-img2 = cv2.threshold(img2, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 # save the the result
-# cv2_imshow(img2)
-cv2.imwrite("output.png", img2)
-# cv2_imshow(img)
-diff = cv2.bitwise_xor(img,img2)
-diff = cv2.bitwise_not(diff)
-cv2_imshow(diff)
-cv2.imwrite("output_nosign.png",diff)
+cv2.imwrite("./outputs/output.png", img)
